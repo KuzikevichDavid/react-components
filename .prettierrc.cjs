7e20a9d113c254dee5ad44d502943d2922226b7b
DavydKuzikevich <wliorjroo@gmail.com>
2023-10-27 16:03:07 +0300

    feat: add router

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,10 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
-    trailingComma: "es5",
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    printWidth: 100,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
 };
 
 module.exports = config;
