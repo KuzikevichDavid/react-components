6774e366eca44fe2ccb5f0bd10701171c8a2d542
DavydKuzikevich <wliorjroo@gmail.com>
2023-11-03 14:57:31 +0300

    feat: add loader

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import TestErrorBoundary from '../components/errorBoundary/TestErrorBoundary';
+import Loader from '../components/loader/Loader';
 import Search from '../components/search/Search';
 
 function Home() {
+  const navigation = useNavigation();
   return (
     <>
       <TestErrorBoundary />
       <Search startPage={1} />
-      <Outlet />
+      {navigation.state === 'loading' ? <Loader /> : <Outlet />}
     </>
   );
 }
